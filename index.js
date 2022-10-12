e9663c8576b3526244eaedbf454c34dc50fdfc02
vostijd <vostijd22@gmail.com>
2022-10-12 10:49:01 -0400

    create getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('fart')
+function getComputerChoice () {
+    let choice = ['Rock','Paper','Scissors'];
+    let random = choice[Math.floor(Math.random()*choice.length)]
+    console.log(random)
+    
+}
