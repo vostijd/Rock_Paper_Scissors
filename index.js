976b529a27ee9052138d36665d46498d46b3b80f
vostijd <vostijd22@gmail.com>
2022-10-12 11:59:11 -0400

    create functions to play a round

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function getComputerChoice () {
-    let choice = ['Rock','Paper','Scissors'];
+    let choice = ['rock','paper','scissors'];
     let random = choice[Math.floor(Math.random()*choice.length)]
-    console.log(random)
-    
+    return random
 }
+
+function getPlayerChoice () {
+    let choice =  prompt ('rock paper or scissors?');
+    return choice.toLowerCase()
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection== 'rock' && computerSelection == 'paper' ||
+    playerSelection == 'scissors' && computerSelection =='rock' ||
+    playerSelection == 'paper' && computerSelection == 'scissors') {
+    let string = 'You Lose! ' + computerSelection + ' beats ' +playerSelection
+       return string
+    } else if (playerSelection== 'rock' && computerSelection == 'scissors' ||
+    playerSelection == 'scissors' && computerSelection =='paper' ||
+    playerSelection == 'paper' && computerSelection == 'rock') {
+    let string = 'You Win! ' + playerSelection + ' beats ' +computerSelection
+       return string
+    } else {
+        let string = 'draw';
+        return string
+    }
+}
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection))
